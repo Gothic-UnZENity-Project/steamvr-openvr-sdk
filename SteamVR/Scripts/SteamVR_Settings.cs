040fc6b4b9ce0fed715c6bbda7d9b8fe9a59969a
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2024-06-06 13:02:26 +0200

    Revert "update(SteamVR_Settings): Added fix from PR https://github.com/ValveSoftware/steamvr_unity_plugin/pull/491"
    This reverts commit 69466bd620e452f8e8294a5e65ab82bb480d7024.

--- SteamVR/Scripts/SteamVR_Settings.cs
+++ SteamVR/Scripts/SteamVR_Settings.cs
@@ -108,12 +108,6 @@ namespace Valve.VR
 
 #if UNITY_EDITOR
                     string localFolderPath = SteamVR.GetSteamVRResourcesFolderPath(true);
-
-                    if (!System.IO.Directory.Exists(localFolderPath)) {
-                        Debug.LogWarning($"<b>[SteamVR]</b> Path '{localFolderPath}' did not exist; creating ...");
-                        System.IO.Directory.CreateDirectory(localFolderPath);
-                    }
-
                     string assetPath = System.IO.Path.Combine(localFolderPath, "SteamVR_Settings.asset");
 
                     UnityEditor.AssetDatabase.CreateAsset(_instance, assetPath);
